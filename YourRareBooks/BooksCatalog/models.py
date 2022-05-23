c102327911aa0023c7be3292c9aacfd300e9db83
Medwedkas <79253741+Medwedkas@users.noreply.github.com>
2022-05-23 18:58:42 +0300

    Create DataBase and AdminPanel

--- YourRareBooks/BooksCatalog/models.py
+++ YourRareBooks/BooksCatalog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Books(models.Model):
+    name = models.CharField('Название', max_length=100)
+    phouse = models.CharField('Издательство', max_length=100)
+    release_date = models.DateField('Дата выпуска')
+    photo = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)
+    available = models.BooleanField(default=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField('Описание')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
