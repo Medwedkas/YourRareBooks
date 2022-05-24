f6a21093002b109e274d1df8906fbddbc80fed54
Medwedkas <79253741+Medwedkas@users.noreply.github.com>
2022-05-24 19:21:55 +0300

    Image fix

--- YourRareBooks/BooksCatalog/admin.py
+++ YourRareBooks/BooksCatalog/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Books
 
 admin.site.register(Books)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phouse', 'release_date', 'available', 'price']
+    list_editable = ['available', 'price']
 # Register your models here.
--- YourRareBooks/BooksCatalog/models.py
+++ YourRareBooks/BooksCatalog/models.py
@@ -5,9 +5,9 @@ class Books(models.Model):
     name = models.CharField('Название', max_length=100)
     phouse = models.CharField('Издательство', max_length=100)
     release_date = models.DateField('Дата выпуска')
-    photo = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)
-    available = models.BooleanField(default=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    photo = models.ImageField('Фото товара', blank=True)
+    available = models.BooleanField('В налии', default=True)
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
     description = models.TextField('Описание')
 
     class Meta:
